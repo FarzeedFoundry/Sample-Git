a7af7890059b4bf9e91b64282c977ab312f98240
FarzeedFoundry <49945374+FarzeedFoundry@users.noreply.github.com>
2019-04-24 14:20:51 +0100

    Add files via upload

---
+++ TestLibrary.cpp
@@ -0,0 +1,14 @@
+#include "TestLibrary.h"
+#include <stdio.h>
+
+TestLibraryNS::TestLibrary::TestLibrary()
+{
+    // Empty constructor
+}
+
+void TestLibraryNS::TestLibrary::Info()
+{
+    // Generate some info
+    printf("This is some information");
+}
+
---
+++ TestLibrary.h
@@ -0,0 +1,16 @@
+ #ifndef _TESTLIBRARY_
+ #define _TESTLIBRARY_
+
+namespace TestLibraryNS
+{
+    class TestLibrary
+    {
+        public:
+            TestLibrary();
+            virtual ~TestLibrary() {};
+
+            void Info();
+    }; // TestLibraryNS  
+}
+
+ #endif
